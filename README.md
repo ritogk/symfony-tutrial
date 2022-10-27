1d9e8a9e067d3de4ec6a366004aba47ccbe6ae8e
Your Name <you@example.com>
2022-10-27 10:35:12 +0900

    work

--- README.md
+++ README.md
@@ -1,11 +1,32 @@
+# symfony cli insatll
+curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | sudo -E bash
+sudo apt install symfony-cli
+
 # project作成
 
 ---
 
 `composer create-project symfony/skeleton:"6.1.*" my_project_directory`
 
-## symfony ビルドイン起動
+## symfony 起動
+symfony server:start -d  
+or  
+`php -S 127.0.0.1:8000 -t public`  
+## symfony server log出力(色付き)
+symfony server:log  
 
----
+## symfony 環境変数出力
+symfony var:export
 
-`php -S 127.0.0.1:8000 -t public`
+## symfony エンティティからmigration作成
+symfony console make:migration
+
+## symfony マイグレーションからDB後進
+symfony console doctrine:migrations:migrate
+
+## php
+# php モジュール確認
+php -m
+
+## pdo_pgsql install
+sudo apt-get install php-pgsql
---
+++ src/Entity/Conference.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConferenceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ConferenceRepository::class)]
+class Conference
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\OneToMany(mappedBy: 'conference', targetEntity: Comment::class, orphanRemoval: true)]
+    private $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setConference($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getConference() === $this) {
+                $comment->setConference(null);
+            }
+        }
+
+        return $this;
+    }
+}
